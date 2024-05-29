3bb5cb1152865a13746a3d131e3cdcdcd4f3e959
oyeonjoo <oyj0842@naver.com>
2024-05-29 22:56:16 +0900

    :sparkles: feat: add output console

---
+++ src/main/java/racingcar/Prompt.java
@@ -0,0 +1,32 @@
+package racingcar;
+
+import java.util.List;
+
+public class Prompt {
+
+    public Prompt() {
+    }
+
+    public void startMessage() {
+        System.out.println(Message.ENTER_RACING_CAR_NAME_MESSAGE.getMessage());
+    }
+
+    public void attemptMessage() {
+        System.out.println(Message.NUMBER_OF_ATTEMPT_MESSAGE.getMessage());
+    }
+
+    public void printResult() {
+        System.out.println(Message.RESULT_MESSAGE.getMessage());
+    }
+
+    public void printFinalWinnerName(List<String> winners) {
+        System.out.println(Message.FINAL_WINNER_MESSAGE.getMessage() + String.join(", ", winners));
+    }
+
+    public void printRoundResult(List<RacingCar> cars) {
+        for (RacingCar car : cars) {
+            System.out.println(car);
+        }
+        System.out.println();
+    }
+}
