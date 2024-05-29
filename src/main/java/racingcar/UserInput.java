5cf5693bbb8d812734f7cbb782652dd7c323edfb
oyeonjoo <oyj0842@naver.com>
2024-05-29 22:56:00 +0900

    :sparkles: feat: add input console

---
+++ src/main/java/racingcar/UserInput.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class UserInput {
+
+    public UserInput() {
+    }
+
+    public String getCarName() {
+        return Console.readLine();
+    }
+
+    public String getAttemptNumber() {
+        return Console.readLine();
+    }
+}
